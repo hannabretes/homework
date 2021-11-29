1828346b72214988ba8bbbec914ac693cfb87c1d
HannaBretes <bretes.hanna@gmail.com>
2021-11-29 18:28:09 +0100

    ref.

--- choinka.py
+++ choinka.py
@@ -1,6 +1,32 @@
-a=input("Wpisz symbol: ")
-print("         " + a)
-print("        " + a * 3)
-print("       " + a * 5)
-print("      " + a * 7)
-print("         " + a)
+a = input("Wpisz symbol: ")
+b = " "
+print(b * 9 + a)
+print(b * 8 + a * 3)
+print(b * 7 + a * 5)
+print(b * 6 + a * 7)
+print(b * 9 + a)
+
+# a = int(input("Podaj wysokość choinki: "))
+# b = input("Wpisz symbol: ")
+# c = " "
+# pietra = int(1)
+# galaz = int(1)
+# while pietra <= a:
+#     print(c * (a-pietra) + b * galaz)
+#     galaz = galaz + 2
+#     pietra = pietra + 1
+
+# pietra = int(1)
+# galaz = int(1)
+# while pietra <= a:
+#     print(c * (a-pietra) + b * galaz)
+#     galaz = galaz + 2
+#     pietra = pietra + 1
+#
+# pietra = int(1)
+# galaz = int(1)
+# while pietra <= a:
+#     print(c * (a-pietra) + b * galaz)
+#     galaz = galaz + 2
+#     pietra = pietra + 1
+#print(c * (a-1) + b)
